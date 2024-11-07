d5a8341ccd35263b0947b44a56385b0013ae5125
ITmail-dk <48917593+ITmail-dk@users.noreply.github.com>
2024-11-07 15:00:05 +0100

    Update install-steam-debian.sh

--- install-steam-debian.sh
+++ install-steam-debian.sh
@@ -21,7 +21,7 @@ sudo apt update
 sudo apt install -y mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
 # nvidia-driver-libs:i386
 
-sudo apt install -y ./steam_latest.deb
+sudo apt install -y ./steam.deb
 
 cd ~
 
